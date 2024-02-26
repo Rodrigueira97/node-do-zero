b1e70b018bd4b66c1b66317513463785deea6bfa
Rodrigueira97 <rodrigoxc97@hotmail.com>
2024-02-26 12:22:24 -0300

    feat: my first steps in node

---
+++ README.md
@@ -0,0 +1,9 @@
+This project was just a first step towards learning node
+
+In it I just simulate a video API where I can send, delete, edit and list my videos
+everything saved in an in-memory database for testing only.
+
+
+To run the project, run the following command
+
+npm run dev
---
+++ server.js
@@ -0,0 +1,51 @@
+import fastify from "fastify"
+import { DatabaseMemory } from "./database-memory.js"
+
+const server = fastify()
+
+const database = new DatabaseMemory()
+
+server.get('/videos', (req) => {
+  const search = req.query.search
+
+  const videos =  database.list(search)
+
+  return videos
+})
+
+server.post('/videos', (req, res) => {
+  const { title, description, duration } = req.body
+
+  database.create({
+    title,
+    description,
+    duration,
+  })
+
+  return res.status(201).send()
+})
+
+server.put('/videos/:id', (req,res) => {
+  const videoId = req.params.id
+  const { title, description, duration } = req.body
+
+  database.update(videoId, {
+    title,
+    description,
+    duration,
+  })
+
+  return res.status(204).send()
+})
+
+server.delete('/videos/:id', (req,res) => {
+  const videoId = req.params.id
+
+  database.delete(videoId)
+
+  return res.status(201).send()
+})
+
+server.listen({
+  port: 3333,
+})
